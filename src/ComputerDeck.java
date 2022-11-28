10e11dc0f75ed41958e31973c16d2b3787763896
Imran Fazilov <ifazilov3584@bths.edu>
2022-11-28 12:27:16 -0500

    day 1 end

--- src/ComputerDeck.java
+++ src/ComputerDeck.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class ComputerDeck {
+import java.util.*;
+
+public class ComputerDeck {
+    private int deckSize;
+    private int[] deckCardVals = {};
+
+    public ComputerDeck() {
+        deckSize = 13;
+    }
+
+    public static int[] addElement(int length, int array[], int element)
+    {
+        int i;
+
+        // create a new array of size n+1
+        int newarray[] = new int[length + 1];
+
+        for (i = 0; i < length; i++)
+            newarray[i] = array[i];
+
+        newarray[length] = element;
+
+        return newarray;
+    }
+
+    public void AssignCards() {
+        int randomNum;
+        for (int i = 1; i <= 13; i++) {
+            randomNum = (int) ((Math.random() * 51) + 1);
+            deckCardVals = addElement(deckCardVals.length, deckCardVals, randomNum);
+        }
+    }
+
+    public String getDeck() {
+        return Arrays.toString(deckCardVals);
+    }
+
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Tester {
+public class Tester {
+    public static void main(String[] args) {
+        ComputerDeck test = new ComputerDeck();
+        test.AssignCards();
+        System.out.println(test.getDeck());
+    }
 }
